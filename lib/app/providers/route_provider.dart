72a8ce321b24846236d69bd685c850f88c91df52
xkritti <krittamet0404@gmail.com>
2023-06-07 08:31:30 +0700

    clone from nylo 5.x

---
+++ lib/app/providers/route_provider.dart
@@ -0,0 +1,15 @@
+import '/routes/router.dart';
+import 'package:nylo_framework/nylo_framework.dart';
+
+class RouteProvider implements NyProvider {
+  @override
+  boot(Nylo nylo) async {
+    nylo.addRouter(appRouter());
+    return nylo;
+  }
+
+  @override
+  afterBoot(Nylo nylo) async {
+    nylo.setInitialRoute(NyRouter.getInitialRoute());
+  }
+}
